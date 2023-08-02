42a6d30333111a5f74c9c3bad668203d5e479c0e
Melissa <70529928+foronmeli@users.noreply.github.com>
2023-08-01 21:04:14 -0500

    Create punto13.py

---
+++ punto13.py
@@ -0,0 +1,12 @@
+numero1=int(input("Digite un numero: "))
+numero2=int(input("Digite otro numero: "))
+suma=numero1+numero2
+resta1=numero1-numero2
+resta2=numero2-numero1
+multi=numero1*numero2
+divi1=numero1/numero2
+divi2=numero2/numero1
+print("La suma es: ",suma)
+print("La resta con el numero1 al incio es: ",resta1,", la resta con el numero2 al incio es: ",resta2)
+print("La multiplicacion es: ",multi)
+print("La division con el numero1 al incio es: ",divi1,", la division con el numero2 al incio es: ",divi2)
